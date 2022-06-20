4782af155084c86bcf13c3cd0876387136897de3
MirasKazik <kazik.m@email.cz>
2022-06-20 14:26:32 +0200

    setup project

---
+++ Project-EUCYON-Java-Tribes-B/src/main/java/com/example/projecteucyonjavatribesb/model/Resources.java
@@ -0,0 +1,31 @@
+package com.example.projecteucyonjavatribesb.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Resources {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private Integer amount;
+    private Integer generation;
+    private Long updatedAt;
+    @ManyToOne
+    @JoinColumn(name = "kingdom")
+    private Kingdom kingdom;
+
+    public Resources(String type, Integer amount, Integer generation, Long updatedAt) {
+        this.type = type;
+        this.amount = amount;
+        this.generation = generation;
+        this.updatedAt = updatedAt;
+    }
+}
