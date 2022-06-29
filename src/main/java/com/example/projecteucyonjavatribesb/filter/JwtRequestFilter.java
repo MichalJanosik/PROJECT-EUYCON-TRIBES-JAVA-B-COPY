b659e6bfceeeb4b000b5eadb5198a7be3aecedd4
Michael <grekmi@email.cz>
2022-06-29 23:45:50 +0200

    Added Jwt Filter and authorization functionality

---
+++ src/main/java/com/example/projecteucyonjavatribesb/filter/JwtRequestFilter.java
@@ -0,0 +1,91 @@
+package com.example.projecteucyonjavatribesb.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.projecteucyonjavatribesb.model.Kingdom;
+import com.example.projecteucyonjavatribesb.repository.KingdomRepository;
+import com.example.projecteucyonjavatribesb.service.PlayerService;
+import com.example.projecteucyonjavatribesb.service.PlayerServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Slf4j
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private PlayerServiceImpl playerService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        if (request.getServletPath().equals("/api/login")){
+            chain.doFilter(request, response);
+        } else {
+            final String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                try {
+                    String username = getUsername(authorizationHeader);
+
+                    if (username != null /*&& SecurityContextHolder.getContext().getAuthentication() == null*/){
+
+                        UserDetails userDetails = this.playerService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken
+                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                    chain.doFilter(request, response);
+                    }
+                } catch (Exception exception) {
+                    log.error("Error logging in : {}", exception.getMessage());
+                    response.setHeader("error", exception.getMessage());
+                    response.setStatus(FORBIDDEN.value());
+                    Map<String, String> error = new HashMap<>();
+                    error.put("error_message", exception.getMessage());
+                    response.setContentType(APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(), error);
+
+
+                }
+            } else {
+                chain.doFilter(request, response);
+            }
+
+        }
+    }
+
+    public String getUsername (String authorizationHeader){
+        String token = authorizationHeader.substring(7);
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        String username = decodedJWT.getSubject();
+        return username;
+    }
+}
+
