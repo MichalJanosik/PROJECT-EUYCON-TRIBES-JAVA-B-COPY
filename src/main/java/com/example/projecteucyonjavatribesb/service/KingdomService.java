9caab03b9162499f375ab2a87512e0a45c7b815e
MichalJanosik <janosik.michal.jm@gmail.com>
2022-07-12 11:08:19 +0200

    EJTB-68 update KingdomService

--- src/main/java/com/example/projecteucyonjavatribesb/service/KingdomService.java
+++ src/main/java/com/example/projecteucyonjavatribesb/service/KingdomService.java
@@ -5,9 +5,13 @@ import com.example.projecteucyonjavatribesb.model.DTO.KingdomNameDTO;
 import com.example.projecteucyonjavatribesb.model.Kingdom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface KingdomService {
     Kingdom findKingdomById(Long id);
     KingdomDTO getKingdomDTO(Long id);
     void renameKingdom(Long kingdomId, KingdomNameDTO kingdomNameDTO);
     KingdomDTO getRenamedKingdomDTO(Long kingdomId);
+    List<Kingdom> findAllKingdoms();
 }
