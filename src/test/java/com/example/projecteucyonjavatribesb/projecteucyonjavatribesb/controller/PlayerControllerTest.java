f4ec61f198b9eb14de6f1fb177ecd74f7dd2cebd
TomasMikula <t.mikula@centrum.cz>
2022-07-15 10:10:00 +0200

    test n.10

--- src/test/java/com/example/projecteucyonjavatribesb/projecteucyonjavatribesb/controller/PlayerControllerTest.java
+++ src/test/java/com/example/projecteucyonjavatribesb/projecteucyonjavatribesb/controller/PlayerControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.projecteucyonjavatribesb.projecteucyonjavatribesb.controller;
 
+import com.example.projecteucyonjavatribesb.controller.PlayerController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -51,7 +55,7 @@ class PlayerControllerTest {
         mockMvc.perform(post("/api/registration")
                         .content("""
                                 {
-                                    "username": " ",
+                                    "username": "",
                                     "password": "password",
                                     "kingdomName": "Aquapark"
                                 }""")
